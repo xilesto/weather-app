3e9f7c401061ab0e8f4515dacaf1419c6df2b33a
xilesto <39354213+xilesto@users.noreply.github.com>
2021-06-03 01:25:35 +0200

    Update app.js

--- CommentsOnlyJS/app.js
+++ CommentsOnlyJS/app.js
@@ -1,4 +1,6 @@
 // Personal API Key for OpenWeatherMap API
+const baseUrl = 'https://api.openweathermap.org/data/2.5/forecast?zip=';
+const apiKey  = '&appid=0af22a241d933cfc136187fa0332baeb';
 
 // Event listener to add function to existing HTML DOM element
 
